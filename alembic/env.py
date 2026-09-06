e8e08bca4231ebb5b4a5b7195b45186c3c343ad3
Zachary-TWH <zacharytangwaihoe@gmail.com>
2026-09-06 22:37:01 +0800

    removedockercompose_now replaced by k8&terra

--- alembic/env.py
+++ alembic/env.py
@@ -2,7 +2,6 @@ from logging.config import fileConfig
 from app.models import Base
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from alembic import context
 
 # this is the Alembic Config object, which provides
